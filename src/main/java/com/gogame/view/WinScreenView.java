a705a62efd9b185145cc0b7275ab19149065abae
Felix <k12043299@students.jku.at>
2023-04-10 15:38:45 +0200

    fixed resize bug, added CSS support, changed layout

--- src/main/java/com/gogame/view/WinScreenView.java
+++ src/main/java/com/gogame/view/WinScreenView.java
@@ -1,27 +1,15 @@
 package com.gogame.view;
 
 import com.gogame.controller.WinScreenController;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
 
 public class WinScreenView extends View {
     //region Fields
-    // Set style of button
-    private final String STYLE = """
-            -fx-background-color:
-                        linear-gradient(#ffd65b, #e68400),
-                        linear-gradient(#ffef84, #f2ba44),
-                        linear-gradient(#ffea6a, #efaa22),
-                        linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%),
-                        linear-gradient(from 0% 0% to 15% 50%, rgba(255,255,255,0.9), rgba(255,255,255,0));
-                -fx-background-radius: 30;
-                -fx-background-insets: 0,1,2,3,0;
-                -fx-text-fill: #654b00;
-                -fx-font-weight: bold;
-                -fx-font-size: 14px;
-                -fx-padding: 10 20 10 20;
-            """;
     private final String winner;
 
     // Pane of this class
@@ -57,12 +45,19 @@ public class WinScreenView extends View {
         pane = new BorderPane();
 
         Text winText = new Text("Player " + winner + " wins!");
+
         Button homeMenu = new Button("Home menu");
-        homeMenu.setStyle(STYLE);
         homeMenu.setOnMouseClicked(e -> controller.changeSceneToStartScreen());
 
+        FlowPane pa = new FlowPane(homeMenu);
+        pa.setAlignment(Pos.CENTER);
+        pa.setPadding(new Insets(30));
+        pa.setHgap(10);
+        pa.setVgap(10);
+
+
         pane.setCenter(winText);
-        pane.setBottom(homeMenu);
+        pane.setBottom(pa);
     }
 
     //endregion
