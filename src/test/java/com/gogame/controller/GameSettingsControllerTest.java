17cd6a214d6cb64120fb3b58e645c317efc3c19f
Nippi0815 <niederberger.dominik@gmx.at>
2023-04-24 16:18:05 +0200

    Create Test classes. No Logic implemented. Add TestFx Framework for Junit5

---
+++ src/test/java/com/gogame/controller/GameSettingsControllerTest.java
@@ -0,0 +1,25 @@
+package com.gogame.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameSettingsControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setBoardSize() {
+    }
+
+    @Test
+    void setHandicap() {
+    }
+
+    @Test
+    void setKomi() {
+    }
+}
---
+++ src/test/java/com/gogame/model/GoBoardModelTest.java
@@ -0,0 +1,78 @@
+package com.gogame.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoBoardModelTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSizes() {
+    }
+
+    @Test
+    void getHandicap() {
+    }
+
+    @Test
+    void getKomi() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void getFields() {
+    }
+
+    @Test
+    void getField() {
+    }
+
+    @Test
+    void getGameState() {
+    }
+
+    @Test
+    void getCapturedByWhite() {
+    }
+
+    @Test
+    void getCapturedByBlack() {
+    }
+
+    @Test
+    void makeMove() {
+    }
+
+    @Test
+    void addGameListener() {
+    }
+
+    @Test
+    void removeGameListener() {
+    }
+
+    @Test
+    void getGameListeners() {
+    }
+
+    @Test
+    void setGameListeners() {
+    }
+}
